4aea9e54794d964d4d0ae2030712748a10e3a9f4
Joan Chepkoech <joan92311@gmail.com>
2025-07-21 02:04:52 +0300

    header amd app layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React  from 'react';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1> Smart Goal Planner</h1>
       </header>
+      <main>
+      <p>
+        You are just a few steps away from achieving your goals!
+        </p>
+      </main>
     </div>
   );
 }
